dc2f3b62063f90df97358e513602f96082863d1b
jensenkhem <boondasoonda@gmail.com>
2022-09-10 11:53:37 -0600

    Added basic driver model

---
+++ ualberta-carpool-backend/ualberta_carpool_backend/api/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+# Create your models here.
+
+class Driver(models.Model):
+    name = models.CharField(max_length=50)
+    longitude = models.FloatField()
+    latitude = models.FloatField()
+    desired_arrival_time = models.TimeField()
+    phone_number = PhoneNumberField(null=False, blank=False, unique=True)
+
